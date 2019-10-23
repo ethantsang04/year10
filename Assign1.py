df0e0824f1c07b9248534248b9196eb54b480c30
ethantsang04 <43586306+ethantsang04@users.noreply.github.com>
2019-10-22 23:40:30 -0400

    Update Assign1.py

--- Assign1.py
+++ Assign1.py
@@ -26,41 +26,43 @@ def writeHTML(datajson,e1,variable):
 	ofile.write("</header>")
 	ofile.write("</body>")
 	ofile.write("<br>")
-	ofile.write("<h3>Below are the average annual temperatures found by 15 global climate models during the years " + str(e1))
-	for i in range(15):
-		ofile.write("<p>" + str(datajson[i]['annualData'][0]) + "</p>")
+	ofile.write("<h3>Below are the average annual temperatures found by 15 global climate models during the years " + str(e1) + ":")
+	for i in range(15): #lists out all 15 variables for temperatures conducted by different GCMs
+		ofile.write("<p>" + str(datajson[i]['annualData'][0]) + "</p>") #json data displayed on html file
 	ofile.write("</html>")
 	ofile.close()
 
-def errorHTML(error):
-	ofile = open("climatedata.html","w")
-	ofile.write("<p>" + str(error) + "</p>")
-	ofile.close()
-
 def getAPI():
+	#sends request to internet
 	myrequest = requests.get("http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/tas/" + str(variable.get())+ "/" + e1.get() + ".json")
 	if (myrequest.status_code == 200):
 		datajson = myrequest.json()
-		annual_data = datajson[0]["annualData"]
+		annual_data = datajson[0]["annualData"] #parsing data
 		writeHTML(datajson,variable.get(),e1.get())
 		b2 = Button(win,text="view data",command=webbrowser.open("file:///Users/ethan.tsang/Documents/Design/climatedata.html"))
+		#Opens html file ^^^
 		b2.grid(row=6,column=1)
 	elif (myrequest.status_code == 400):
 		messagebox.showinfo("Error 400", "Error 400 has occured. Client provided incorrect parameters for the request.")
+		#error message if improper input
 
+#Starts by opening tkinter interface
 win = Tk()
 win.geometry('425x400')
 win.title('type a country code')
 
+#Heading
 Label_1 = Label(win, text="Enter country(ex//. CAN, USA, etc): ", font="Verdana 18 bold")
 Label_1.grid(row=1,column=2)
 
+#Entry box for countries
 e1 = Entry(win)
 e1.grid(row=2,column=2)
 
 Label_2 = Label(win, text="Choose a time interval:", font="Verdana 18 bold")
 Label_2.grid(row=3,column=2)
 
+#dropdown menu for selecting years
 variable = StringVar(win)
 variable.set("1940/1959")
 
